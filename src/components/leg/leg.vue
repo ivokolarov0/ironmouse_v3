fee61421e3c230421ab10861b65bffc72ec6934a
codearttube <codearttube@gmail.com>
2021-11-20 17:15:37 +0200

    Add leg strap

--- src/components/leg/leg.vue
+++ src/components/leg/leg.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="leg" :class="{ 'is-right': right }">
+    <div class="leg-straps" v-if="!right">
+      <div class="leg-straps-line leg-straps-line--top"></div>
+      <div class="leg-straps-line leg-straps-line--bottom"></div>
+      <div class="leg-straps__cirlce">
+        <div class="leg-straps__cirlce-inner"></div>
+      </div>
+      <div class="leg-straps__belt leg-straps__belt--left">
+        <div class="leg-straps__belt-inner">
+          <div class="legs-straps__belt-item">
+            <div class="leg-straps__belt-hole"></div>
+          </div>
+        </div>
+      </div>
+      <div class="leg-straps__belt leg-straps__belt--right">
+        <div class="leg-straps__belt-inner">
+          <div class="legs-straps__belt-item">
+            <div class="leg-straps__belt-hole"></div>
+          </div>
+        </div>
+      </div>
+      <div class="leg-straps__overlay"></div>
+    </div>
     <div class="leg__bottom">
       <div class="leg__inner">
         <div class="leg__side">
