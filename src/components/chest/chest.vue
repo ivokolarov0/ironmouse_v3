aaa00f40c33a8058c5bc080945cd6dca67790f95
codearttube <codearttube@gmail.com>
2021-11-22 22:35:20 +0200

    Chest wip

--- src/components/chest/chest.vue
+++ src/components/chest/chest.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="chest"></div>
-  <!-- /.chest -->
+  <div class="chest">
+    <div class="chest-wrapper">
+      <div class="chest__inner">
+        <div class="chest__holder"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
