99ee5be2768ef2930b79626b4a7f32c8bf81d0e2
codearttube <codearttube@gmail.com>
2021-11-24 00:08:30 +0200

    Chest area wip

--- src/components/chest/chest.vue
+++ src/components/chest/chest.vue
@@ -2,7 +2,43 @@
   <div class="chest">
     <div class="chest-wrapper">
       <div class="chest__inner">
-        <div class="chest__holder"></div>
+        <div class="chest__holder">
+          <div class="sweater">
+            <div class="cleavage">
+              <div class="cleavage-item"></div>
+              <div class="cleavage-item"></div>
+            </div>
+            <div class="sweater-top">
+              <div class="sweater-top__inner">
+                <div class="sweater-top__line-left">
+                  <div class="sweater-top__line-inner"></div>
+                </div>
+                <div class="sweater-top__line-right">
+                  <div class="sweater-top__line-inner"></div>
+                </div>
+                <div class="sweater-top__line-center">
+                  <div class="sweater-top__line-inner">
+                    <div class="sweater-top__line-center-item"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="sweater__inner"></div>
+            <div class="breast-area">
+              <div class="breast breast--left"></div>
+              <div class="breast breast--right"></div>
+              <div class="breast-area-center">
+                <div class="breast-area-center__inner"></div>
+              </div>
+            </div>
+            <div class="sweater-side-bottom sweater-side-bottom--left">
+              <div class="sweater-side-bottom__inner"></div>
+            </div>
+            <div class="sweater-side-bottom sweater-side-bottom--right">
+              <div class="sweater-side-bottom__inner"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
