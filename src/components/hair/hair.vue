82c7e12137189facfe14b0a713ef3aac7a7dca6f
codearttube <codearttube@gmail.com>
2021-12-11 22:23:24 +0200

    Hair wip

--- src/components/hair/hair.vue
+++ src/components/hair/hair.vue
@@ -8,6 +8,35 @@
             <div class="hair-loop__circle2"></div>
           </div>
         </div>
+        <div class="hair-loop">
+          <div class="hair-loop__inner">
+            <div class="hair-loop__circle1-wrapper">
+              <div class="hair-loop__circle1"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="hair-parts1">
+        <div class="hair-part">
+          <div class="hair-part__inner"></div>
+        </div>
+        <div class="hair-part">
+          <div class="hair-part__inner"></div>
+        </div>
+        <div class="hair-part">
+          <div class="hair-part__inner"></div>
+        </div>
+        <div class="hair-part">
+          <div class="hair-part__inner"></div>
+        </div>
+        <div class="hair-part">
+          <div class="hair-part__inner"></div>
+        </div>
+        <div class="hair-part">
+          <div class="hair-part__inner">
+            <div class="hair-part__side"></div>
+          </div>
+        </div>
       </div>
     </div>
     <div class="hair__side">
@@ -17,6 +46,33 @@
             <div class="hair-loop__circle1"></div>
             <div class="hair-loop__circle2"></div>
           </div>
+        </div>
+        <div class="hair-loop">
+          <div class="hair-loop__inner">
+            <div class="hair-loop__circle1-wrapper">
+              <div class="hair-loop__circle1"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="hair-parts1">
+        <div class="hair-part">
+          <div class="hair-part__inner"></div>
+        </div>
+        <div class="hair-part">
+          <div class="hair-part__inner"></div>
+        </div>
+        <div class="hair-part">
+          <div class="hair-part__inner"></div>
+        </div>
+        <div class="hair-part">
+          <div class="hair-part__inner"></div>
+        </div>
+        <div class="hair-part">
+          <div class="hair-part__inner"></div>
+        </div>
+        <div class="hair-part">
+          <div class="hair-part__inner"></div>
         </div>
       </div>
     </div>
